bad4e9d887362e11e62a38aa00093afd440d4f86
Ricky Darmawan <rickydarmawan201@gmail.com>
2022-09-06 13:35:39 +0700

    first commit

---
+++ src/pages/Start/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './start.css'
+
+class Start extends React.Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className='start-page'>
+                <h1><span>e</span>Learn</h1>
+                <button>Mulai Belajar</button>
+            </div>
+        )
+    }
+}
+
+export default Start
